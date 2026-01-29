92a083da792795a78d0e46122aac26acfc105908
Jaydeep0342 <jaydeep0342@gmail.com>
2026-01-29 15:11:53 +0530

    card project

--- airplane/src/App.jsx
+++ airplane/src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import Card from './component/card'
-const App = () => {
-  return (
-    <div className='parent'>
-    < Card user ='ram' age ={10}  img="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YWlycGxhbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" />
-    <Card  user='aman' age ={12} img = "https://images.unsplash.com/photo-1769407642563-8773cc64995e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"/>
-     </div>
-  )
+// import React from 'react'
+// import Card from './component/card'
+// const App = () => {
+//   return (
+//     <div className='parent'>
+//     < Card user ='ram' age ={10}  img="https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YWlycGxhbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" />
+//     <Card  user='aman' age ={12} img = "https://images.unsplash.com/photo-1769407642563-8773cc64995e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D"/>
+//      </div>
+//   )
   
+// }
+// export default App
+import React from 'react'
+import Card from './component/card.jsx'
+function App() {
+  return (
+    <div className=' parent '>
+    <Card/>
+    <Card/>
+    <Card/>
+      </div>
+  )
 }
+
 export default App
 
