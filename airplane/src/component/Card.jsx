55dc56dc426e43587db0a68d98944250e5a98477
Jaydeep0342 <jaydeep0342@gmail.com>
2026-01-30 15:40:55 +0530

    props

--- airplane/src/component/Card.jsx
+++ airplane/src/component/Card.jsx
@@ -16,21 +16,22 @@ const Card = (props) => {
         <div className="card">
             <div>
                 <div className="top">
-                    <img src="https://images.unsplash.com/photo-1649734926695-1b1664e98842?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YW1hem9uJTIwbG9nb3xlbnwwfHwwfHx8MA%3D%3D" alt="nachioooooo" />
+                    <img src={props. brandLogo} alt="logo" />
                     <button>Save <Bookmark size={10} /> </button>
                 </div>
                 <div className="center">
-                    <h3>Amazon <span>14 hour ago</span></h3>
-                    <h2>java devloper</h2>
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}
+                    </h2>
                     <div className='tag'>
-                        <h4>part time </h4>
-                        <h4>full time</h4>
+                        <h4>{props.tag1} </h4>
+                        <h4>{props.tag2}</h4>
                     </div>
                 </div>
             </div>
             <div className="bottom">
                 <div>
-                    <h3>$123/h</h3>
+                    <h3>{props.pay}</h3>
                     <p>Mumbai, India</p>
                 </div>
                 <button>Apply Now</button>
--- airplane/src/component/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-  return (
-    <div className='nav'> </div>
-  )
-}
-
-export default Navbar
