787b5a721e0fe00c4d748414982c52213f26366f
Jaydeep0342 <jaydeep0342@gmail.com>
2026-02-05 16:16:09 +0530

    batch is usestate

---
+++ advancedUsestate/vite-project/src/App.jsx
@@ -0,0 +1,37 @@
+// import React, { useState } from 'react'
+
+// const App = () =>{
+//   const [count, setCount] = useState([10,20,30])
+//  const btnclick = () =>{  
+//  const newCount = [...count]
+// newCount.push(40)
+// setCount(newCount)
+// }
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={btnclick}> click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+const App = () => {
+  const[num, setNum] = React.useState(0)
+  const btnclick = () =>{
+    setNum(...)
+    setNum()
+    setNum()
+  }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
