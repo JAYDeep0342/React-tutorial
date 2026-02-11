ee52ca03fe5e5a3a0c41742a3f7493313d73feb7
Jaydeep0342 <jaydeep0342@gmail.com>
2026-02-11 13:42:29 +0530

    use-effect hooks

---
+++ USE-EFFECT/vite-project/src/App.jsx
@@ -0,0 +1,33 @@
+// import React, { use } from 'react'
+// import { useEffect } from 'react' 
+// function App() {
+//   const[num, setNum] = React.useState(0)
+//   const[num2, setNum2] = React.useState(100)
+   
+//   useEffect(() => {
+//     console.log('useEffect')
+//   }, [num])
+//   return (
+// <div>
+// <h1>num1:{num}</h1>   
+// <h1>num2:{num2}</h1>
+// <button onMouseEnter={() => setNum(num + 1)} 
+//   onMouseLeave={() => setNum2(num2 + 1)}
+//   >click me</button>
+//  </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
