7d63e0319ca5064c801273672fe1c54fd1fca028
Jaydeep0342 <jaydeep0342@gmail.com>
2026-02-16 21:11:52 +0530

    chils as props

---
+++ Bonous-Topic/vite-project/src/components/Nav2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nav2 = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Nav2;
---
+++ Context-Api/vite-project/src/context/ThemeContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ThemeContext = () => {
+    return (
+        <div>heloo</div>
+    )
+}
+
+export default ThemeContext;
